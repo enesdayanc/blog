ea38dda4009cd2608bb1ad3ec7bfae766f5953c3
enesdayanc <enesdayanc@gmail.com>
2017-02-15 21:51:01 +0300

    add FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sp\BowerBundle\SpBowerBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
         );
 
