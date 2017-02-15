ad154942ffc944bc9867e81914b95861b9c8d1f1
enesdayanc <enesdayanc@gmail.com>
2017-02-16 00:32:01 +0300

    add createPost and editPost pages

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Post;
+use AppBundle\Form\PostType;
+use AppBundle\Services\PostService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -19,5 +22,77 @@ class AdminController extends Controller
     public function indexAction(Request $request)
     {
         return array();
+    }
+
+    /**
+     * @Route("/post/create", name="create_post")
+     * @Template(":admin:form.html.twig")
+     */
+    public function createPostAction(Request $request)
+    {
+        $post = new Post();
+
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            /** @var PostService $postService */
+            $postService = $this->get('app.services.post_service');
+
+            $saveResult = $postService->savePost($post);
+
+            if ($saveResult) {
+
+                $redirectUrl = $this->generateUrl(
+                    'edit_post',
+                    array(
+                        'id' => $post->getId()
+                    )
+                );
+
+                return $this->redirect($redirectUrl);
+            }
+        }
+
+        return array(
+            'form' => $form->createView(),
+        );
+    }
+
+    /**
+     * @Route("/post/edit/{id}", name="edit_post")
+     * @Template(":admin:form.html.twig")
+     */
+    public function editPostAction($id, Request $request)
+    {
+        /** @var PostService $postService */
+        $postService = $this->get('app.services.post_service');
+
+        $post = $postService->getPostById($id);
+
+        if (!$post) {
+            // todo: log this
+            throw $this->createNotFoundException('Unable to find this entity.');
+        }
+
+        $form = $this->createForm(PostType::class, $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $saveResult = $postService->savePost($post);
+
+            if ($saveResult) {
+                // todo: show success message
+            } else {
+                // todo: show error message
+            }
+        }
+
+
+        return array(
+            'form' => $form->createView(),
+        );
     }
 }
