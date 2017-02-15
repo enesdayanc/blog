95f7d6775fb00792104957c5c8fe4bb20f94f5f9
enesdayanc <enesdayanc@gmail.com>
2017-02-15 23:30:30 +0300

    add admin homage

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,9 +2,22 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @Route("/admin")
+ */
 class AdminController extends Controller
 {
+    /**
+     * @Route("/", name="admin_homepage")
+     * @Template("@App/admin/index.html.twig")
+     */
+    public function indexAction(Request $request)
+    {
+        return array();
+    }
 }
